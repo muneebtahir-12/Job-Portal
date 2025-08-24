35407d96b5aea5b91633c34d137ead7bc90ea954
Muhammad Muneeb Tahir <tmuneeb090@gmail.com>
2025-08-24 21:35:47 +0500

    Added signup form

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-        document.querySelector('.offers select').addEventListener('change', function() {
-            const selectedValue = this.value;
-            document.querySelectorAll('.front-end').forEach(function(frontEnd) {
-                frontEnd.style.display = 'none';
-                if (frontEnd.classList.contains(selectedValue)) {
-                    frontEnd.style.display = 'flex';
-                }
-                if (selectedValue === 'all') {
-                    frontEnd.style.display = 'flex';
-                }
-            });
-        });
+document.querySelector('.offers select').addEventListener('change', function() {
+const selectedValue = this.value;
+document.querySelectorAll('.front-end').forEach(function(frontEnd) {
+    frontEnd.style.display = 'none';
+    if (frontEnd.classList.contains(selectedValue)) {
+        frontEnd.style.display = 'flex';
+     }
+    if (selectedValue === 'all') {
+         frontEnd.style.display = 'flex';
+     }
+       });
+    });
+
+const signupBtn = document.querySelector('.links button');
+const modal = document.getElementById('signup');
+const closeBtn = document.querySelector('.close-btn');
+signupBtn.addEventListener('click', function() {
+  modal.style.display = 'flex';
+});
+closeBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+document.querySelector('.signup-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Sign up successful!');
+  modal.style.display = 'none';
+});
